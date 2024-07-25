36219facd1e815c738b4115a13d71a722dafce2b
heiwa4126 <heiwa4126@gmail.com>
2024-07-25 15:57:51 +0900

    add biome and reformat sources

--- test/hono-awslambda1.test.ts
+++ test/hono-awslambda1.test.ts
@@ -4,14 +4,13 @@
 
 // example test. To run these tests, uncomment this file along with the
 // example resource in lib/hono-awslambda1-stack.ts
-test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new HonoAwslambda1.HonoAwslambda1Stack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
-
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+test("SQS Queue Created", () => {
+	//   const app = new cdk.App();
+	//     // WHEN
+	//   const stack = new HonoAwslambda1.HonoAwslambda1Stack(app, 'MyTestStack');
+	//     // THEN
+	//   const template = Template.fromStack(stack);
+	//   template.hasResourceProperties('AWS::SQS::Queue', {
+	//     VisibilityTimeout: 300
+	//   });
 });
